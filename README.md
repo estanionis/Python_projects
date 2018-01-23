4bdc4e4098edba157ff75f3fcf8b113307a9a030
undo11 <estanionis@gmail.com>
2018-01-23 22:28:20 +0200

    Staciakampio plotas

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Python_projects
-# Just testing
--- work1/work1.py
+++ work1/work1.py
@@ -1,2 +1,8 @@
-#! /usr/bin/env python
-print ("Hello World!")
+#! /usr/bin/env python3
+'''
+Programa skaičiuojanti stačiakampio plotą
+'''
+REIKSME = input('Iveskite pirmos krastines ilgi = ')
+REIKSME2 = input('Iveskite antros krastines ilgi = ')
+print('Staciakampio plotas =', float(REIKSME) * float(REIKSME2))
+input('Press enter to close a program ')
