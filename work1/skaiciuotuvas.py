ee9a7489662c13c8fa2c61970e6cce8a7cacc3c7
undo11 <estanionis@gmail.com>
2018-01-23 23:39:37 +0200

    Dvieju zenklu skaiciuotuvas

---
+++ work1/skaiciuotuvas.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+'''
+Programa skaičiuojanti skaičius
+'''
+SKAICIUS = input('Iveskite pirma skaiciu ')
+SKAICIUS1 = input('Iveskite antra skaiciu ')
+ZENKLAS = input('Iveskite zenkla ')
+if str(ZENKLAS) == '+':
+    print('Suma lygi =', float(SKAICIUS) + float(SKAICIUS1))
+elif str(ZENKLAS) == '-':
+    print('Suma lygi =', float(SKAICIUS) - float(SKAICIUS1))
+input('Press enter to close program ')
--- work1/staciakampis.py
+++ work1/staciakampis.py
@@ -5,4 +5,4 @@ Programa skaičiuojanti stačiakampio plotą
 REIKSME = input('Iveskite pirmos krastines ilgi = ')
 REIKSME2 = input('Iveskite antros krastines ilgi = ')
 print('Staciakampio plotas =', float(REIKSME) * float(REIKSME2))
-input('Press enter to close a program ')
+input('Press enter to close program ')
