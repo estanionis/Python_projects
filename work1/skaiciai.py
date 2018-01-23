6521cf3863c751dd3fb70d83e69ec20c2e1b03e3
undo11 <estanionis@gmail.com>
2018-01-24 00:26:57 +0200

    Paprastieji skaiciai

--- work1/skaiciai.py
+++ work1/skaiciai.py
@@ -2,5 +2,8 @@
 '''
 Programa suskaiciuojanti paprastuosius skaicius
 '''
-
+SKAICIUS = input('Iveskite skaiciu = ')
+while SKAICIUS != 0:
+    SKAICIUS = int(SKAICIUS) - 1
+if SKAICIUS
 input('Press enter to close program ')
