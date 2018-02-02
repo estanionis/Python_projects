45d7ac838958dd91402e0bf67d09bd6caf567d24
undo11 <estanionis@gmail.com>
2018-02-02 22:49:41 +0200

    Sveikieji skaiciai

--- work1/skaiciai.py
+++ work1/skaiciai.py
@@ -5,7 +5,7 @@ Programa suskaiciuojanti paprastuosius skaicius
 SKAICIUS = input('Iveskite skaiciu = ')
 DALMUO = 1
 while DALMUO <= int(SKAICIUS):
-    if SKAICIUS % DALMUO == 0:
+    if int(SKAICIUS) % int(DALMUO) == 0:
         print(DALMUO)
     DALMUO = DALMUO + 1
 input('Press enter to close program ')
