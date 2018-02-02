f1b02c3c0021114992f80e45f1a60c079559cf62
undo11 <estanionis@gmail.com>
2018-02-02 22:52:31 +0200

    Staciakampio plotas

--- work1/staciakampis.py
+++ work1/staciakampis.py
@@ -4,5 +4,5 @@ Programa skaiciuojanti keturkampio plota
 '''
 REIKSME = input('Iveskite pirmos krastines ilgi = ')
 REIKSME2 = input('Iveskite antros krastines ilgi = ')
-print('Staciakampio plotas =', float(REIKSME) * float(REIKSME2))
+print ('Staciakampio plotas =', float(REIKSME) * float(REIKSME2))
 input('Press enter to close program ')
