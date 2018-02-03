dfa8fedd657b8771140c4b14958c016c184320cd
undo11 <estanionis@gmail.com>
2018-02-03 13:32:56 +0200

    Tuple keitimas

--- work2/test.py
+++
@@ -1,9 +0,0 @@
-#! usr/bin/env python3
-"""
-Testavimo programa
-"""
-TUPLE1 = (1, )
-TUPLE2 = (1, )
-NOT_A_TUPLE = (1)
-print(TUPLE1 == TUPLE2,'\n',TUPLE2 == NOT_A_TUPLE,'\n', type(NOT_A_TUPLE),'\n', NOT_A_TUPLE)
-input('Press enter to close program')
---
+++ work2/tuple.py
@@ -0,0 +1,11 @@
+#! usr/bin/env python3
+"""
+Programa perrasanti nauja tuple
+"""
+TUPLE1 = (1, 10, 'door', 'warm', True, False, None)
+NEW_TUPLE = TUPLE2 = ()
+for ITEM in TUPLE1:
+    if not isinstance(ITEM, int):
+        NEW_TUPLE += (ITEM,)
+print(TUPLE1,'\n', NEW_TUPLE)
+input('Press enter to close program')
