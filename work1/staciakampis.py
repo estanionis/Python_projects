9d73ba835ccdbfa6f37d9856e6aa375448163132
estanionis <estanionis@gmail.com>
2018-02-06 22:50:18 +0200

    Staciakampis

--- work1/staciakampis.py
+++ work1/staciakampis.py
@@ -5,4 +5,3 @@ Programa skaiciuojanti keturkampio plota
 REIKSME = input('Iveskite pirmos krastines ilgi = ')
 REIKSME2 = input('Iveskite antros krastines ilgi = ')
 print('Staciakampio plotas =', float(REIKSME) * float(REIKSME2))
-input('Press enter to close program ')
