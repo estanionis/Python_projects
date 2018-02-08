f9fef1a39a24b6b36ee5355bad14b1b34532f42b
Edvinas Stanionis <estanionis@gmail.com>
2018-02-08 11:27:13 +0200

    Skaiciuotuvas

--- work1/skaiciuotuvas.py
+++ work1/skaiciuotuvas.py
@@ -1,7 +1,4 @@
 #! /usr/bin/env python3
-'''
-Programa skaiciuojanti dvieju zenklu suma
-'''
 SKAICIUS = input('Iveskite pirma skaiciu ')
 SKAICIUS1 = input('Iveskite antra skaiciu ')
 ZENKLAS = input('Iveskite zenkla ')
